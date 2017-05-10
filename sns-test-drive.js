962ac260858d86913b35c3611398603e4fa1a181
benone <ben1one@gmail.com>
2017-05-10 16:14:39 +0800

    Demo Update
    We will only maintain the jQuery independent sns_nojQ.js

--- sns-test-drive.js
+++ sns-test-drive.js
@@ -8,9 +8,9 @@ $sns.site.getUrl = function(){return window.location.href;};
 $sns.site.getTitle = function(){return "Title1"};
 $sns.site.getDesc = function(){return "Desc1"};
 $sns.site.getImage = function(){
-	if(jQuery('body').hasClass('mkt-cn')){
-		return "http://media.ef.com.cn/~/media/efcom/oct/2014/bg.jpg";
-	}
+	//if(jQuery('body').hasClass('mkt-cn')){
+	//	return "http://media.ef.com.cn/~/media/efcom/oct/2014/bg.jpg";
+	//}
 	return "http://media.ef.com/~/media/efcom/oct/2014/bg.jpg";
 };
 
@@ -18,9 +18,9 @@ $sns.result.getUrl = function(){return window.location.href;};
 $sns.result.getTitle = function(){return "Title2"};
 $sns.result.getDesc = function(){return "Desc2"};
 $sns.result.getImage = function(){
-	if(jQuery('body').hasClass('mkt-cn')){
-		return "http://media.ef.com.cn/~/media/efcom/oct/2014/bg.jpg";
-	}
+	//if(jQuery('body').hasClass('mkt-cn')){
+	//	return "http://media.ef.com.cn/~/media/efcom/oct/2014/bg.jpg";
+	//}
 	return "http://media.ef.com/~/media/efcom/oct/2014/bg.jpg";
 };
 
