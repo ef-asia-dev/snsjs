9537880a7d626b3d170b4fc5609aecf32ebcac4c
benone <ben1one@gmail.com>
2017-05-16 17:23:21 +0800

    IE9

--- sns_nojQ.js
+++ sns_nojQ.js
@@ -1,26 +1,26 @@
 var util = {
-
-  $ : function(el) {
+	
+	$ : function(el) {
 		return document.querySelector(el);
 	},
-  hasClass :function (el, className) {
-		return el.classList ? el.classList.contains(className) : new RegExp(/(^| )${className}( |$)/, 'gi').test(el.className);
+	hasClass :function (el, className) {
+		return el.classList ? el.classList.contains(className) : new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
 	},
-  addClass:function (el, className) {
+	addClass:function (el, className) {
 		if (el.classList) {
 			el.classList.add(className);
 			} else {
-			el.className += ' ' +className;
+			el.className += ' ' + className;
 		}
 	},
-  removeClass:function (el, className) {
+	removeClass:function (el, className) {
 		if (el.classList) {
 			el.classList.remove(className);
 			} else {
-			el.className = el.className.replace(new RegExp(/(^|\\b)${className.split(' ').join('|')}(\\b|$)/, 'gi'), ' ');
+			el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
 		}
 	},
-  closest:function (el, clazz) {
+	closest:function (el, clazz) {
         while ('.'+el.className != clazz) {
             el = el.parentNode;
             if (!el) {
@@ -29,11 +29,11 @@ var util = {
 		}
         return el;
 	}
-
+	
 };
 
 var $sns = {
-
+	
 	//For sharing the site
 	site: {
 		getUrl: function(){return window.location.href.split('?')[0];},
@@ -134,7 +134,7 @@ var $sns = {
 		if(this.loaded){
 			alert('singleton');
 		return;}
-
+		
 		if (this.mobilecheck()) {
 			var wraps = document.querySelectorAll(this.settings.selectors.device.wrapper);
 			for (var i = 0; i < wraps.length; i++) {
@@ -147,12 +147,12 @@ var $sns = {
 			}
 		}
 		this.hideElement();
-
+		
 		if( ! (util.hasClass(util.$('body'), 'mkt-cn')) && !this.settings.disableFB ){
 			this.loadFbAPI();
 			}else{
 		}
-
+		
 		//Fix popup block
 		var obj = this.settings.selectors.sns;
 		for (var prop in obj) {
@@ -164,11 +164,11 @@ var $sns = {
 				}
 			}
 		}
-
+		
 		this.buildLink();
 		this.bindLink();
 		this.bindAction();
-
+		
 		this.loaded = true;
 	},
 	buildLink: function() {
@@ -222,40 +222,40 @@ var $sns = {
 	bindLink: function(){
 		var _this = this;
 		var obj = this.settings.selectors.sns;
-
+		
 		//Share Site
 		for (var key in obj) {
 			if (obj.hasOwnProperty(key)) {
 				var sel =  _this['settings']['selectors']['share']['site'] +" "+_this['settings']['selectors']['sns'][key];
-        var _list = document.querySelectorAll(sel);
-        for (var i = 0; i < _list.length; i++) {
-          if( _list[i] !== null){
-            _list[i].href = _this['site']['links'][key];
-          }
-        }
-
+				var _list = document.querySelectorAll(sel);
+				for (var i = 0; i < _list.length; i++) {
+					if( _list[i] !== null){
+						_list[i].href = _this['site']['links'][key];
+					}
+				}
+				
 			}
 		}
-
+		
 		//Share Result
 		for (var key in obj) {
 			if (obj.hasOwnProperty(key)) {
 				var sel =  _this['settings']['selectors']['share']['result'] +" "+_this['settings']['selectors']['sns'][key];
-        var _list = document.querySelectorAll(sel);
-        for (var i = 0; i < _list.length; i++) {
-          if( _list[i] !== null){
-            _list[i].href = _this['result']['links'][key];
-          }
-        }
+				var _list = document.querySelectorAll(sel);
+				for (var i = 0; i < _list.length; i++) {
+					if( _list[i] !== null){
+						_list[i].href = _this['result']['links'][key];
+					}
+				}
 			}
 		}
-
+		
 	},
 	bindAction: function(){
 		var _this = this;
-
+		
 		var obj = this.settings.selectors.sns;
-
+		
 		for (var key in obj) {
 			if (obj.hasOwnProperty(key)) {
 				//----
@@ -288,7 +288,7 @@ var $sns = {
 					description: _this.result.getDesc(),
 					caption: 'ef.com',
 				}, function(response){});
-
+				
 			}, false);
 		}
 		//----
@@ -302,16 +302,16 @@ var $sns = {
 		//----
 		var _wechat = document.querySelectorAll(_this['settings']['selectors']['sns']['wechat']);
 		for (var i = 0; i < _wechat.length; i++) {
-
+			
 			_wechat[i].addEventListener('click', function (e) {
 				e.preventDefault();
 				_this.buildLink();
 				_this.bindLink();
-
+				
 				var title = "";
 				var url = "";
 				var img = "";
-
+				
 				if( util.closest(this, _this['settings']['selectors']['share']['site']) ){
 					title = _this.site.getTitle();
 					url = _this.site.getUrl();
@@ -322,39 +322,39 @@ var $sns = {
 					url = _this.result.getUrl();
 					img = _this.result.getImage();
 				}
-
+				
 				if (_this.mobilecheck()) {
 					if (navigator.userAgent.toLowerCase().indexOf("micromessenger") == -1) {
 						//browser
-
+						
 						} else {
 						//weixin
 						document.title = title;
-
+						
 						if (window.history && window.history.pushState){
 							history.pushState("", "", url);
 						}
-
+						
 						var _img = document.createElement('img');
 						_img.src = _this.result.getImage();
 						_img.style.width = "0px";
 						_img.style.height = "0px";
 						util.$('body').insertBefore(_img, util.$('body').firstChild);
-
+						
 						alert(decodeURI('%E8%AF%B7%E7%82%B9%E5%8F%B3%E4%B8%8A%E8%A7%92%E5%88%86%E4%BA%AB%E5%88%B0%E6%9C%8B%E5%8F%8B%E5%9C%88'));
 					}
 					}else{
 					window.open(this.getAttribute('href'), "mywindow", "menubar=1,resizable=1,width=800,height=570");
 				}
-
+				
 			}, false);
-
+			
 		}
-
+		
 	},
 	hideElement : function(){
 		var _this = this;
-
+		
 		//Hide disabled SNS
 		//blacklist
 		var obj = this.settings.selectors.sns;
@@ -365,24 +365,24 @@ var $sns = {
 					_this['settings']['blacklist'][mkt] = [];
 				}
 				var arr = _this['settings']['blacklist'][mkt];
-
+				
 				if( arr.indexOf(key) != -1 ){
 					//console.log("Hide "value);
 					_this.cssHide(_this['settings']['selectors']['sns'][key]);
 				}
 			}
 		}
-
+		
 		//WeChat in Webview : Show only WeChat
 		if (_this.isMicromessenger()) {
-
+			
 			//Sharing image hack
 			var _img = document.createElement('img');
 			_img.src = _this.result.getImage();
 			_img.style.width = "0px";
 			_img.style.height = "0px";
 			util.$('body.mkt-cn').insertBefore(_img, util.$('body.mkt-cn').firstChild);
-
+			
 			var obj = this.settings.selectors.sns;
 			for (var key in obj) {
 				if (obj.hasOwnProperty(key)) {
@@ -391,14 +391,14 @@ var $sns = {
 					}
 				}
 			}
-
+			
 		}
-
+		
 		//
 		if (_this.mobilecheck()) {
 			if ( _this.isMicromessenger() ) {
 				//weixin
-
+				
 				} else {
 				//browser
 				_this.cssHide(_this['settings']['selectors']['sns']['wechat']);
@@ -410,8 +410,8 @@ var $sns = {
 			_this.cssHide(_this['settings']['selectors']['sns']['kakaotalk']);
 		}
 		//
-
-
+		
+		
 	},
 	cssHide : function(ele){
 		var css = document.createElement("style");
@@ -434,13 +434,13 @@ var $sns = {
 		}
 		var check = false;
 		(function (a) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera);
-
+		
 		if(check){
 			this.device = "mobile";
 			}else{
 			this.device = "desktop";
 		};
-
+		
 		return check;
 	},
 	isMicromessenger : function(){
@@ -468,7 +468,7 @@ var $sns = {
 				version    : 'v2.9'
 			});
 		};
-
+		
 		(function(d, s, id){
 			var js, fjs = d.getElementsByTagName(s)[0];
 			if (d.getElementById(id)) {return;}
